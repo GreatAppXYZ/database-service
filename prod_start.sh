f1549cae29d78f8efd621955028be4508c8f13d7
alayor <alayor3@gmail.com>
2017-07-21 18:18:47 -0500

    Fixing discovery and configuration services URLs

--- prod_start.sh
+++ prod_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 java $JAVA_OPTS \
     -Dserver.port=$PORT \
-    -Deureka.client.service-url.defaultZone=https://greatappxyz-discovery-service.herokuapp.com/eureka/ \
+    -Deureka.client.service-url.defaultZone=https://greatapp-discovery-service.herokuapp.com/eureka/ \
     -Deureka.instance.hostname=database.greatapp.xyz \
     -Deureka.instance.prefer-ip-address=false \
-    -Dspring.cloud.config.uri=https://greatappxyz-configuration-service.herokuapp.com/ \
+    -Dspring.cloud.config.uri=https://greatapp-configuration-service.herokuapp.com/ \
     -jar app.jar \
