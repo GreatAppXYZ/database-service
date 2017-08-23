085916dcf0f6c5fdcdb01d6e704e3afa088218de
alayor <alayor3@gmail.com>
2017-08-22 21:15:55 -0500

    Moving insert execution to libs-database

--- src/main/java/xyz/greatapp/database/services/DatabaseServiceImpl.java
+++ src/main/java/xyz/greatapp/database/services/DatabaseServiceImpl.java
@@ -1,14 +1,11 @@
 package xyz.greatapp.database.services;
 
-import com.google.common.collect.ObjectArrays;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import xyz.greatapp.database.api.interfaces.DatabaseService;
 import xyz.greatapp.libs.database.adapter.DatabaseAdapterFactory;
-import xyz.greatapp.libs.database.queries.Insert;
-import xyz.greatapp.libs.database.queries.Select;
-import xyz.greatapp.libs.database.queries.SelectList;
+import xyz.greatapp.libs.database.queries.*;
 import xyz.greatapp.libs.database.util.DbBuilder;
 import xyz.greatapp.libs.service.Environment;
 import xyz.greatapp.libs.service.ServiceResult;
@@ -80,61 +77,13 @@ public class DatabaseServiceImpl extends BaseServiceImpl implements DatabaseServ
 
     @Override
     public ServiceResult update(UpdateQueryRQ query) throws Exception {
-        long updatedRows = getDatabaseAdapter().executeUpdate(new DbBuilder() {
-            @Override
-            public String sql() throws SQLException {
-                return "UPDATE " + getSchema() + query.getTable()
-                        + " SET " + addSets(query.getSets())
-                        + addWhere(query.getFilters());
-            }
-
-            @Override
-            public ColumnValue[] values() {
-                return ObjectArrays.concat(query.getSets(), query.getFilters(), ColumnValue.class);
-            }
-
-            @Override
-            public JSONObject build(ResultSet resultSet) throws Exception {
-                return null;
-            }
-        });
-        if (updatedRows == 0) {
-            return new ServiceResult(false, "none.rows.were.updated");
-        }
-        return new ServiceResult(true, "", Long.toString(updatedRows));
-    }
-
-    private String addSets(ColumnValue[] sets) {
-        StringBuilder sb = new StringBuilder();
-        for (ColumnValue filter : sets) {
-            sb.append(filter.getColumn()).append(" = ?, ");
-        }
-        sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, "");
-        return sb.toString();
+        return new Update(getDatabaseAdapter(), getSchema(), query)
+                .execute();
     }
 
     @Override
     public ServiceResult delete(DeleteQueryRQ query) throws Exception {
-        long updatedRows = getDatabaseAdapter().executeUpdate(new DbBuilder() {
-            @Override
-            public String sql() throws SQLException {
-                return "DELETE FROM " + getSchema() + query.getTable()
-                        + addWhere(query.getFilters());
-            }
-
-            @Override
-            public ColumnValue[] values() {
-                return query.getFilters();
-            }
-
-            @Override
-            public JSONObject build(ResultSet resultSet) throws Exception {
-                return null;
-            }
-        });
-        if (updatedRows == 0) {
-            return new ServiceResult(false, "none.rows.were.updated");
-        }
-        return new ServiceResult(true, "", Long.toString(updatedRows));
+        return new Delete(getDatabaseAdapter(), getSchema(), query)
+                .execute();
     }
 }
